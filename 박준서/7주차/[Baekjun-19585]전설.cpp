d3fd26ba4dc0f9b1578bfd735c641dbfa431ed04
parkjs82 <000parkjs@gmail.com>
2025-03-08 23:12:35 +0900

    [Baekjoon-20057]마법사상어와토네이도

--- 박준서/7주차/[Baekjun-19585]전설.cpp
+++ 박준서/7주차/[Baekjun-19585]전설.cpp
@@ -3,21 +3,25 @@
 #include <set>
 using namespace std;
 
+// Trie 구조체 정의
 struct Trie {
  public:
-  bool isEndOfString;
-  map<char, Trie *> next;
+  bool isEndOfString;      // 문자열의 끝을 나타내는 플래그
+  map<char, Trie *> next;  // 다음 문자에 대한 포인터를 저장하는 맵
 
+  // 생성자
   Trie() {
     isEndOfString = false;
     next.clear();
   }
+  // 소멸자 - 메모리 누수 방지를 위해 모든 자식 노드 삭제
   ~Trie() {
     for (auto &it : next) {
       delete it.second;
     }
   }
 
+  // 문자열을 Trie에 삽입하는 함수
   void push(string input) {
     Trie *nowNode = this;
     for (int i = 0; i < input.size(); i++) {
@@ -31,9 +35,10 @@ struct Trie {
 
 string str;
 int N, M, K;
-Trie *color = new Trie;
-set<string> team;
+Trie *color = new Trie;  // 색상을 저장할 Trie
+set<string> team;        // 팀 이름을 저장할 set
 
+// 입력된 문자열이 유효한 이름인지 확인하는 함수
 bool naming(string input) {
   Trie *colorNode = color;
   set<int> point;
